481e76effcae7828c53df01a4f452119efb81c7c
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2026-07-23 15:56:19 +0100

    chore: clean up voronoi test

--- tests/voronoi_test.py
+++ tests/voronoi_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from synthetmic import DiagramConfig, VoronoiDiagramGenerator, VoronoiEvent
+from synthetmic import DiagramConfig, VoronoiDiagramGenerator
 from synthetmic.data import toy
 
 
@@ -29,19 +29,3 @@ def test_pos_and_vols(
     vdg.fit(DiagramConfig(seeds=seeds, domain=domain, periodic=None))
 
     assert np.allclose(vdg.get_fitted_volumes(), expected_vols)
-
-
-@pytest.mark.parametrize("damp_param", (0.0, 0.5, 1.0))
-@pytest.mark.parametrize("n_iter", (10, 20, 30, 40, 50, 100))
-def test_centroid_error_norm_monotonicity(
-    damp_param: float, n_iter: int, config: DiagramConfig
-) -> None:
-    errors = np.zeros(n_iter)
-
-    def callback(e: VoronoiEvent) -> None:
-        errors[e.iteration - 1] = e.centroid_error_norm
-
-    vdg = VoronoiDiagramGenerator(n_iter=n_iter, damp_param=damp_param)
-    vdg.fit(config, callback=callback)
-
-    assert np.all(np.diff(errors) <= 0) is np.True_
